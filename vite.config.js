b344c0986c14b2ee88b73bcc473b14a47c5aca11
squalidfall <you@example.com>
2025-04-17 22:15:27 -0400

    Fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/retirement-app-pages/', // Replace with your actual repo name
+  base: '/retirement-app/', // Replace with your actual repo name
   plugins: [react()],
 })
